4ee8b9d26e516e76767c54b8f093a09001359d9a
echozyr2001 <echo.zyr.2001@gmail.com>
2022-11-18 17:23:50 +0800

    update

--- os3/src/syscall/process.rs
+++ os3/src/syscall/process.rs
@@ -1,7 +1,9 @@
 //! Process management syscalls
 
 use crate::config::{MAX_APP_NUM, MAX_SYSCALL_NUM};
-use crate::task::{exit_current_and_run_next, suspend_current_and_run_next, TaskStatus};
+use crate::task::{
+    exit_current_and_run_next, get_task_info, suspend_current_and_run_next, TaskStatus,
+};
 use crate::timer::get_time_us;
 
 #[repr(C)]
@@ -45,5 +47,6 @@ pub fn sys_get_time(ts: *mut TimeVal, _tz: usize) -> isize {
 
 /// YOUR JOB: Finish sys_task_info to pass testcases
 pub fn sys_task_info(ti: *mut TaskInfo) -> isize {
-    -1
+    get_task_info(ti);
+    0
 }
